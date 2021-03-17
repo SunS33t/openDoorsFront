f6d0da4903092c838c0976e7d7347148062c069d
HOME <buslaew.ar@mail.ru>
2021-03-17 16:33:26 +0400

    Chat realization

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core'
 
+export const backendHost:string = 'localhost:44315'
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
